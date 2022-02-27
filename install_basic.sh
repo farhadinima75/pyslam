a7737e3dff10e28ba5a121e86156a0e481c5d99c
farhadinima75 <farhadinima75@email.kntu.ac.ir>
2022-02-27 16:20:44 +0330

    Update install_basic.sh

--- install_basic.sh
+++ install_basic.sh
@@ -48,3 +48,43 @@ fi
 if [ $INSTALL_CPP -eq 1 ]; then
     ./install_cpp.sh                
 fi 
+
+
+
+
+#set -e
+. bash_utils.sh 
+# ====================================================
+print_blue '================================================'
+print_blue "Building Thirdparty"
+print_blue '================================================'
+STARTING_DIR=`pwd`  # this should be the main folder directory of the repo
+# ====================================================
+# N.B.: this script requires that you have first run:
+#./install_basic.sh 
+# ====================================================
+if [[ -z "${USE_PYSLAM_ENV}" ]]; then
+    USE_PYSLAM_ENV=0
+fi
+if [ $USE_PYSLAM_ENV -eq 1 ]; then
+    . pyenv-activate.sh
+fi  
+# ====================================================
+# check if we have external options
+EXTERNAL_OPTION=$1
+if [[ -n "$EXTERNAL_OPTION" ]]; then
+    echo "external option: $EXTERNAL_OPTION" 
+fi
+if [[ -n "$WITH_PYTHON_INTERP_CHECK" ]]; then
+    echo "WITH_PYTHON_INTERP_CHECK: $WITH_PYTHON_INTERP_CHECK " 
+    EXTERNAL_OPTION="$EXTERNAL_OPTION -DWITH_PYTHON_INTERP_CHECK=$WITH_PYTHON_INTERP_CHECK"
+fi
+# ====================================================
+CURRENT_USED_PYENV=$(get_virtualenv_name)
+print_blue "currently used pyenv: $CURRENT_USED_PYENV"
+
+print_blue "=================================================================="
+print_blue "Configuring and building thirdparty/orbslam2_features ..."
+cd thirdparty/orbslam2_features
+. build.sh $EXTERNAL_OPTION
+cd $STARTING_DIR
